0c284b7f7ea7b25daaa3c9dda794669aea16d6ad
Mathilde Runge <maro@html24.net>
2020-04-27 10:35:56 +0200

    auto-commit

---
+++ inrainorshine_weatherapp/precache-manifest.11fbea2974f043c823dbe7b6212f1c80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887b8b27becd3863fdd53419e0c09b9b",
+    "url": "/inrainorshine_weatherapp/index.html"
+  },
+  {
+    "revision": "c7b9c08a739b48bf3d65",
+    "url": "/inrainorshine_weatherapp/static/css/main.2c97d726.chunk.css"
+  },
+  {
+    "revision": "43984b0dc5a402255d9e",
+    "url": "/inrainorshine_weatherapp/static/js/2.c75eee3e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/inrainorshine_weatherapp/static/js/2.c75eee3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7b9c08a739b48bf3d65",
+    "url": "/inrainorshine_weatherapp/static/js/main.cc27c72a.chunk.js"
+  },
+  {
+    "revision": "9e4a851eefcb17cf89db",
+    "url": "/inrainorshine_weatherapp/static/js/runtime-main.5b71739c.js"
+  }
+]);
